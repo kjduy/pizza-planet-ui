26c9e57dcbef165e3a68b02a677f17396ebee334
kjduy <k.vn.jdh@gmail.com>
2023-02-23 22:57:09 -0500

    PIZ15: Modify date view on order detail page

--- js/order/order-detail.js
+++ js/order/order-detail.js
@@ -10,6 +10,8 @@ fetch(`http://127.0.0.1:5000/order/id/${_id}`)
   .then((order) => {
     let template = createRowTemplate(order);
     $("#order").append(template);
+    formatDateTime(order.date, "formatted-date", "date");
+    formatDateTime(order.date, "formatted-time", "time");
   });
 
 /**
@@ -19,4 +21,19 @@ fetch(`http://127.0.0.1:5000/order/id/${_id}`)
 function createRowTemplate(order) {
   let template = $("#order-template")[0].innerHTML;
   return Mustache.render(template, order);
+}
+
+function formatDateTime(dateTime, elementId, formatType) {
+  const inputDate = new Date(dateTime);
+  const differenceMinutes = new Date().getTimezoneOffset();
+  const utcTime = inputDate.getTime() - differenceMinutes * 60000;
+  const utcDate = new Date(utcTime);
+  const options = {
+    dateStyle: formatType === "date" ? "short" : undefined,
+    timeStyle: formatType === "time" ? "short" : undefined,
+  };
+  const formatter = new Intl.DateTimeFormat("en-US", options);
+  const formattedDateTime = formatter.format(utcDate);
+  const formattedElement = document.getElementById(elementId);
+  formattedElement.textContent = formattedDateTime;
 }
